a16553af2d6c1c039101d87f877813a494e5166d
John Lee <john.cylee@gmail.com>
2016-09-22 16:58:03 +0800

    Update build-image.sh to include commit hash in image tag

--- contrib/docker/build-image.sh
+++ contrib/docker/build-image.sh
@@ -3,10 +3,12 @@
 set -e
 
 WORKDIR=/go/src/github.com/4freewifi/smshinet
+COMMIT=$(git rev-parse --short HEAD)
 
 docker run --rm -v "$PWD":"$WORKDIR" \
     -w "${WORKDIR}/smshinetd" \
     golang:1-alpine  \
     go build -o smshinetd-alpine -v
 
-docker build -t smshinetd -f contrib/docker/Dockerfile .
+docker build -t smshinetd:${COMMIT} -f contrib/docker/Dockerfile .
+docker tag smshinetd:${COMMIT} smshinetd:latest
